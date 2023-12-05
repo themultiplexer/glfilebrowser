af028dd89b2aed25cb7bb5a900add6efd78fe96b
themultiplexer <josh@rutschmann.tech>
2023-12-05 14:03:03 +0100

    Optimizations ongoing

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <string>
 #include <iostream>
+#include <filesystem>
 #define FMT_HEADER_ONLY
 #include <fmt/core.h>
 #include <fmt/ranges.h>
@@ -18,10 +19,20 @@
 
 using namespace std::chrono;
 using namespace cimg_library;
+namespace fs = std::filesystem;
 
 
-void GLAPIENTRY
-MessageCallback( GLenum source,
+class GLobj {
+  public:
+    GLuint vao;
+    GLuint vbo;
+    GLuint textures;
+    unsigned int vertices;
+};
+
+GLobj directory;
+
+void GLAPIENTRY MessageCallback( GLenum source,
                  GLenum type,
                  GLuint id,
                  GLenum severity,
@@ -66,19 +77,223 @@ void PrintShaderInfoLog(GLint const Shader)
 	}
 }
 
+void create_hull(std::vector<GLfloat> *vertices, std::vector<GLfloat> *bary, std::vector<GLuint> *indices, float x_offset, unsigned int index){
+	float cube_width = 20.0f;
+	float cube_height = 10.0f;
+	float cube_depth = 10.0f;
+
+	std::vector<GLfloat> v = {
+		-cube_width,cube_depth + x_offset,  cube_height,//0
+		cube_width,cube_depth + x_offset,   cube_height,//2
+		cube_width,x_offset,  cube_height,//3
+		-cube_width,cube_depth + x_offset,  cube_height,//0
+		cube_width,x_offset,  cube_height,//3
+		-cube_width,x_offset, cube_height,//1
+		cube_width,cube_depth + x_offset,   cube_height,//2
+		cube_width,cube_depth + x_offset,  0.0f,//6
+		cube_width,x_offset, 0.0f,//7
+		cube_width,cube_depth + x_offset,   cube_height,//2
+		cube_width,x_offset, 0.0f,//7
+		cube_width,x_offset,  cube_height,//3
+		-cube_width,cube_depth + x_offset, 0.0f,//4 
+		-cube_width,x_offset,0.0f,//5
+		-cube_width,cube_depth + x_offset,  cube_height,//0
+		-cube_width,cube_depth + x_offset,  cube_height,//0
+		-cube_width,x_offset,0.0f,//5
+		-cube_width,x_offset, cube_height,//1
+	};
+
+	std::vector<GLfloat> b = {
+		0.0f, 1.0f,//0
+		1.0f, 1.0f,//2
+		0.0f, 1.0f,//3
+		0.0f, 1.0f,//0
+		0.0f, 1.0f,//3
+		1.0f, 1.0f,//1
+		1.0f, 1.0f,//2
+		1.0f, 0.0f,//6
+		0.0f, 0.0f,//7
+		1.0f, 1.0f,//2
+		0.0f, 0.0f,//7
+		0.0f, 1.0f,//3
+		0.0f, 0.0f,//4
+		1.0f, 0.0f,//5
+		0.0f, 1.0f,//0
+		0.0f, 1.0f,//0
+		1.0f, 0.0f,//5
+		1.0f, 1.0f,//1
+	};
+
+	for ( int i = 0; i < 6; i++) {
+		indices->push_back(1);
+	}
+	for ( int i = 0; i < 6; i++) {
+		indices->push_back(index);
+	}
+	for ( int i = 0; i < 6; i++) {
+		indices->push_back(index + 1);
+	}
+
+	std::copy(std::begin(v), std::end(v), std::back_inserter(*vertices));
+	std::copy(std::begin(b), std::end(b), std::back_inserter(*bary));
+}
+
+GLuint buildTextureArray(std::vector<std::filesystem::directory_entry> files){
+	int width = 640, height = 480;
+	GLsizei count = files.size(); 
+
+	GLuint texture3D;
+	glGenTextures(1, &texture3D);
+	glBindTexture(GL_TEXTURE_2D_ARRAY, texture3D);
+
+	glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_RGB, width, height, count + 2, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+
+	CImg<unsigned char> floor("../test.png");
+	floor.resize(width, height);
+	floor.permute_axes("cxyz");
+	glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, 0, width, height, 1, GL_RGB, GL_UNSIGNED_BYTE, floor.data());
+	glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, 1, width, height, 1, GL_RGB, GL_UNSIGNED_BYTE, floor.data());
+
+
+	CImg<unsigned char> folder_icon("../folder.png");
+	CImg<unsigned char> file_icon("../file.png");
+	CImg<unsigned char> image(width,height,1,3);
+	//
+	int i = 2;
+	for (std::filesystem::directory_entry file : files)
+	{
+		//image.clear();
+		image.permute_axes("yzcx");
+		
+		cimg_forXY(image,x,y) {
+			image(x,y,0,0)=255;
+			image(x,y,0,1)=0;
+			image(x,y,0,2)=255;
+		}
+		unsigned char cyan[]    = {0,   255, 255 };
+		unsigned char black[]   = {0,   0,   0   };
+		unsigned char yellow[]  = {255, 255, 0   };
+		if (file.is_directory()) {
+			image.draw_image(50, 50, folder_icon);
+			image.draw_text(30,60, file.path().c_str(), black,cyan,1,64);
+		} else {
+			if (file.path().extension() == ".png" || file.path().extension() == ".jpg") {
+				image = CImg<unsigned char>(file.path().c_str());
+				image.resize(width, height);
+				image.draw_text(30,60, file.path().c_str(), black,yellow,1,64);
+			} else {
+				image.draw_image(50, 50, file_icon);
+				image.draw_text(30,60, file.path().c_str(), black,yellow,1,64);
+			}
+		}
+		
+		image.permute_axes("cxyz");
+		glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, i, width, height, 1, GL_RGB, GL_UNSIGNED_BYTE, image.data());
+		i++;
+	}
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D_ARRAY, 0);
+	return texture3D;
+}
+
+GLobj buildDirectory(std::string path) {
+	std::vector<std::filesystem::directory_entry> files;
+    for (const auto & entry : fs::directory_iterator(path)){
+        std::cout << entry.path() << std::endl;
+		files.push_back(entry);
+	}
+
+	GLobj data;
+
+	std::vector<GLfloat> vertices;
+	std::vector<GLfloat> bary;
+	std::vector<GLuint> indices;
+
+	float floor_width = 20.0f;
+	float floor_length = files.size() * 11.0 / 2.0;
+
+	GLfloat floorvertices[] = {
+		-floor_width, floor_length, 0.0f,
+		floor_width, floor_length, 0.0f,
+		-floor_width, 0.0f, 0.0f,
+		floor_width, floor_length, 0.0f,
+		floor_width, 0.0f, 0.0f,
+		-floor_width, 0.0f, 0.0f,
+	};
+	GLfloat texCoords[] = {
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		0.0f, 1.0f,
+		1.0f, 0.0f,
+		1.0f, 1.0f,
+		0.0f, 1.0f,
+	};
+	GLuint texIndex[] = {0,0,0,0,0,0};
+
+	std::copy(std::begin(floorvertices), std::end(floorvertices), std::back_inserter(vertices));
+	std::copy(std::begin(texCoords), std::end(texCoords), std::back_inserter(bary));
+	std::copy(std::begin(texIndex), std::end(texIndex), std::back_inserter(indices));
+
+	for (int i = 0; i < files.size() / 2; i++) {
+		create_hull(&vertices, &bary, &indices, (float) i * 11.0f, i * 2 + 2);
+	}
+
+	GLuint barybuffer;
+	glGenBuffers(1, &barybuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, barybuffer);
+	glBufferData(GL_ARRAY_BUFFER, bary.size() * sizeof(GLfloat), &bary[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	GLuint indexbuffer;
+	glGenBuffers(1, &indexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, indexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+	glGenVertexArrays(1, & data.vao);
+	glBindVertexArray(data.vao);
+	glGenBuffers(1, & data.vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, data.vbo);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	glBindVertexArray(data.vao);
+	glBindBuffer(GL_ARRAY_BUFFER, data.vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, barybuffer);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, indexbuffer);
+	glVertexAttribIPointer(2, 1, GL_UNSIGNED_INT, GL_FALSE, 0);
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+    std::vector<CImg<unsigned char>> images;
+    data.textures = buildTextureArray(files);
+	data.vertices = vertices.size();
+	return data;
+}
+
 const unsigned int SCR_WIDTH = 3840;
 const unsigned int SCR_HEIGHT = 2560;
 
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
-float yaw         = -90.0f;
-float pitch       =  0.0f;
+float yaw  = 0.0f;
+float pitch  =  0.0f;
 
 glm::vec3 cameraPos   = glm::vec3(-0.01f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 1.0f, 0.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 0.0f, 1.0f);
-
 glm::vec3 walkDirection = glm::vec3(0.0f, 1.0f, 0.0f);
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
@@ -127,7 +342,7 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
     float cameraSpeed = 0.5f; // adjust accordingly
     if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
-        cameraSpeed *= 2.0;
+        cameraSpeed *= 2.5;
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         cameraPos += cameraSpeed * walkDirection;
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -141,33 +356,16 @@ void processInput(GLFWwindow *window)
 
 	if (cameraPos.z > 3.0)
 		cameraPos.z -= 0.1;
-}
 
-GLuint loadTexture(const char * imagepath){
-    int w;
-    int h;
-    int comp;
-    unsigned char* image = stbi_load(imagepath, &w, &h, &comp, STBI_rgb_alpha);
-
-    if(image == nullptr) {
-      throw(std::string("Failed to load texture"));
-    }
-
-    // Create one OpenGL texture
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    // Return the ID of the texture we just created
-    return textureID;
+	if (abs(cameraPos.x) > 20.5) {
+		cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+		cameraFront = glm::vec3(0.0f, 1.0f, 0.0f);
+		walkDirection = glm::vec3(0.0f, 1.0f, 0.0f);
+		firstMouse = true;
+		yaw = 0.0f;
+		pitch = 0.0f;
+		directory = buildDirectory("../../");
+	}
 }
 
 GLuint CreateShader(char const *vert, char const *frag) {
@@ -249,110 +447,8 @@ void create_box(std::vector<GLfloat> *vertices, std::vector<GLuint> *indices){
 	std::copy(std::begin(i), std::end(i), std::back_inserter(*indices));
 }
 
-void create_hull(std::vector<GLfloat> *vertices, std::vector<GLfloat> *bary, std::vector<GLuint> *indices, float x_offset, unsigned int index){
-	float cube_width = 20.0f;
-	float cube_height = 5.0f;
-	float cube_depth = 5.0f;
+int main() {
 
-	std::vector<GLfloat> v = {
-		-cube_width,cube_depth + x_offset,  cube_height * 2.0f,//0
-		cube_width,cube_depth + x_offset,   cube_height * 2.0f,//2
-		cube_width,-cube_depth + x_offset,  cube_height * 2.0f,//3
-		-cube_width,cube_depth + x_offset,  cube_height * 2.0f,//0
-		cube_width,-cube_depth + x_offset,  cube_height * 2.0f,//3
-		-cube_width,-cube_depth + x_offset, cube_height * 2.0f,//1
-		cube_width,cube_depth + x_offset,   cube_height * 2.0f,//2
-		cube_width,cube_depth + x_offset,  0.0f,//6
-		cube_width,-cube_depth + x_offset, 0.0f,//7
-		cube_width,cube_depth + x_offset,   cube_height * 2.0f,//2
-		cube_width,-cube_depth + x_offset, 0.0f,//7
-		cube_width,-cube_depth + x_offset,  cube_height * 2.0f,//3
-		-cube_width,cube_depth + x_offset, 0.0f,//4 
-		-cube_width,-cube_depth + x_offset,0.0f,//5
-		-cube_width,cube_depth + x_offset,  cube_height * 2.0f,//0
-		-cube_width,cube_depth + x_offset,  cube_height * 2.0f,//0
-		-cube_width,-cube_depth + x_offset,0.0f,//5
-		-cube_width,-cube_depth + x_offset, cube_height * 2.0f,//1
-	};
-
-	std::vector<GLfloat> b = {
-		0.0f, 1.0f,//0
-		1.0f, 1.0f,//2
-		0.0f, 1.0f,//3
-		0.0f, 1.0f,//0
-		0.0f, 1.0f,//3
-		1.0f, 1.0f,//1
-		1.0f, 1.0f,//2
-		1.0f, 0.0f,//6
-		0.0f, 0.0f,//7
-		1.0f, 1.0f,//2
-		0.0f, 0.0f,//7
-		0.0f, 1.0f,//3
-		0.0f, 0.0f,//4
-		1.0f, 0.0f,//5
-		0.0f, 1.0f,//0
-		0.0f, 1.0f,//0
-		1.0f, 0.0f,//5
-		1.0f, 1.0f,//1
-	};
-
-	for ( int i = 0; i < 6; i++) {
-		indices->push_back(0);
-	}
-	for ( int i = 0; i < 6; i++) {
-		indices->push_back(index);
-	}
-	for ( int i = 0; i < 6; i++) {
-		indices->push_back(index + 1);
-	}
-
-	std::copy(std::begin(v), std::end(v), std::back_inserter(*vertices));
-	std::copy(std::begin(b), std::end(b), std::back_inserter(*bary));
-}
-
-GLuint buildTextureArray()
-	{
-
-		int width = 640, height = 480;
-		GLsizei count = 10; 
-
-		GLuint texture3D;
-		glGenTextures(1, &texture3D);
-		glBindTexture(GL_TEXTURE_2D_ARRAY, texture3D);
-
-		//glPixelStorei(GL_UNPACK_ROW_LENGTH, width);
-		//glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-		glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_RGB, width, height, count, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-
-		int i = 0;
-		for (int i = 0; i < count; i++)
-		{
-			// Create 640x480 image
-			CImg<unsigned char> image(width,height,1,3);
-			cimg_forXY(image,x,y) {
-				image(x,y,0,0)=255;
-				image(x,y,0,1)=0;
-				image(x,y,0,2)=255;
-			}
-			unsigned char cyan[]    = {0,   255, 255 };
-			unsigned char black[]   = {0,   0,   0   };
-			unsigned char yellow[]  = {255, 255, 0   };
-			image.draw_text(30,60, fmt::format("Black 64pt on cyan {}!", i).c_str(),black,cyan,1,64);
-			image.draw_text(80,200,"Yellow 32pt on black semi-transparent",yellow,black,0.5,32);
-			image.save_png(fmt::format("{}.png", i).c_str());
-			image.permute_axes("cxyz");
-			//printf("%s\n", &image.data());
-			glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, i, width, height, 1, GL_RGB, GL_UNSIGNED_BYTE, image.data());
-		}
-		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glBindTexture(GL_TEXTURE_2D_ARRAY, 0);
-		return texture3D;
-	}
-
-int main()
-{
 	GLFWwindow* window;
 	milliseconds start = duration_cast< milliseconds >(
 		system_clock::now().time_since_epoch()
@@ -423,137 +519,17 @@ int main()
 		}
 	)GLSL";
 
-	char const * VertexShaderSource2 = R"GLSL(
-		#version 330 core
-		layout (location = 0) in vec3 aPos;
-		layout (location = 1) in vec2 aTexCoord;
-
-		uniform mat4 model;
-		uniform mat4 view;
-		uniform mat4 proj;
-
-		out vec2 TexCoord;
-
-		void main()
-		{
-			gl_Position = proj * view * model * vec4(aPos, 1.0);
-			TexCoord = aTexCoord;
-		}
-	)GLSL";
-
-	char const * FragmentShaderSource2 = R"GLSL(
-		#version 330
-		in vec2 TexCoord;
-		uniform sampler2D ourTexture;
-
-		void main()
-		{
-			gl_FragColor = texture(ourTexture, TexCoord);
-			//gl_FragColor = vec4(TexCoord, 0.0, 1.0);
-		}
-	)GLSL";
-
-	std::vector<GLfloat> vertices;
-	std::vector<GLfloat> bary;
-	std::vector<GLuint> indices;
-
-	for (int i = 0; i < 5; i++) {
-		create_hull(&vertices, &bary, &indices, (float) i * 20.0f, i * 2);
-	}
-
-	fmt::print("{}\n", indices);
-
-	float floor_width = 50.0f;
-	float floor_height = 50.0f;
-
-	GLfloat vertices2[] = {
-		-floor_width, -floor_width, 0.0f, 0.0f, 0.0f,
-		floor_width, -floor_width, 0.0f, 1.0f, 0.0f,
-		-floor_width, floor_width, 0.0f, 0.0f, 1.0f,
-		floor_width, -floor_width, 0.0f, 1.0f, 0.0f,
-		floor_width, floor_width, 0.0f, 1.0f, 1.0f,
-		-floor_width, floor_width, 0.0f, 0.0f, 1.0f,
-	};
-
-	GLuint barybuffer;
-	glGenBuffers(1, &barybuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, barybuffer);
-	glBufferData(GL_ARRAY_BUFFER, bary.size() * sizeof(GLfloat), &bary[0], GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	
-	GLuint indexbuffer;
-	glGenBuffers(1, &indexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, indexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	
-	GLuint VAO;
-	glGenVertexArrays(1, & VAO);
-	glBindVertexArray(VAO);
-	GLuint VBO;
-	glGenBuffers(1, & VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	
-	GLuint VAO2;
-	glGenVertexArrays(1, & VAO2);
-	glBindVertexArray(VAO2);
-	GLuint VBO2;
-	glGenBuffers(1, & VBO2);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
 	GLuint ShaderProgram = CreateShader(VertexShaderSource, FragmentShaderSource);
-	GLuint ShaderProgram2 = CreateShader(VertexShaderSource2, FragmentShaderSource2);
-
-	//glUseProgram(ShaderProgram);
-
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	
-	glBindBuffer(GL_ARRAY_BUFFER, barybuffer);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, indexbuffer);
-	glVertexAttribIPointer(2, 1, GL_UNSIGNED_INT, GL_FALSE, 0);
-	glEnableVertexAttribArray(2);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	glBindVertexArray(VAO2);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
 
 	glUseProgram(ShaderProgram);
 	update_camera(ShaderProgram);
 	glUseProgram(0);
 
-	glUseProgram(ShaderProgram2);
-	update_camera(ShaderProgram2);
-	GLuint floorTexture = loadTexture("../test.png");
-	glUseProgram(0);
-
-
-    std::vector<CImg<unsigned char>> images;
-    GLuint textureArray = buildTextureArray();
+	directory = buildDirectory("../");
 
 	glEnable(GL_TEXTURE_2D_ARRAY);
 	glEnable(GL_DEPTH_TEST);
+
 	// During init, enable debug output
 	glEnable              ( GL_DEBUG_OUTPUT );
 	glDebugMessageCallback( MessageCallback, 0 );
@@ -570,24 +546,12 @@ int main()
 		glUniform1f(glGetUniformLocation(ShaderProgram, "time"), (float)time);
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		glBindTexture(GL_TEXTURE_2D_ARRAY, textureArray);   
-		glBindVertexArray(VAO);
-
-		glDrawArrays(GL_TRIANGLES, 0, vertices.size() * sizeof(GLfloat));
+		glBindTexture(GL_TEXTURE_2D_ARRAY, directory.textures);   
+		glBindVertexArray(directory.vao);
+		glDrawArrays(GL_TRIANGLES, 0, directory.vertices * sizeof(GLfloat));
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
 		glBindTexture(GL_TEXTURE_2D_ARRAY, 0);
-		glUseProgram(0);
-
-
-		glUseProgram(ShaderProgram2);
-		glBindTexture(GL_TEXTURE_2D, floorTexture);
-		glUniformMatrix4fv(glGetUniformLocation(ShaderProgram2, "view"), 1, GL_FALSE, glm::value_ptr(view));
-		glBindVertexArray(VAO2);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		glBindVertexArray(0);
-		glBindTexture(GL_TEXTURE_2D, 0);
 		glUseProgram(0);
 
 		glfwSwapBuffers(window);
@@ -596,8 +560,8 @@ int main()
 
 	glDeleteProgram(ShaderProgram);
 
-	glDeleteBuffers(1, & VBO);
-	glDeleteVertexArrays(1, & VAO);
+	glDeleteBuffers(1, & directory.vbo);
+	glDeleteVertexArrays(1, & directory.vao);
 
 	glfwTerminate();
 	return 0;
